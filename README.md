e4f1efb757ef1b7e7637b67bf9bca5492c7bed7a
Santiago <santiagoamarilla2000@gmail.com>
2021-12-21 21:32:44 -0300

    Initial commit based on advances on previous repository

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # barradar
-Looking for nearby bars? You're in luck
+Looking for nearby bars? You're in luck. 
+
+This is another restart of the barradar project, but starting from scratch instead of a template AND without react because styling gets more complex than it needs to, I will do a react-based project later on.
---
+++ js/maps.js
@@ -0,0 +1,10 @@
+console.log("hi");
+
+var map = L.map('map',{
+    center: [43.64701, -79.39425],
+    zoom: 15
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
